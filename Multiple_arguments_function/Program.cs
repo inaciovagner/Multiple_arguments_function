8b98d201975911542b78bfa80a6137e4e148cf6c
Vagner de Souza <inaciovagner1@gmail.com>
2021-04-26 14:36:03 +0100

    Add project files.

---
+++ Multiple_arguments_function/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace CsharpPrograms
+{
+    class Program
+    {
+        static public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        static public int Add(int a, int b, int c = 0)
+
+        {
+            return a + b + c;
+        }
+        static void Main(string[] args)
+        {
+            int x, y, z, result = 0;
+            x = Convert.ToInt32(Console.ReadLine());
+            y = Convert.ToInt32(Console.ReadLine());
+            z = Convert.ToInt32(Console.ReadLine());
+            if (z > 0 || z < 0)
+            {
+                result = Add(x, y, z);
+            }
+            else
+            {
+                result = Add(x, y);
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
